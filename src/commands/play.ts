0ceebf93701d4d5f2429ee40883883cce7006073
shyrack <CodeBucketCore@gmail.com>
2021-12-07 23:09:31 +0100

    added initial code for play command

--- src/commands/play.ts
+++ src/commands/play.ts
@@ -1,12 +1,20 @@
 import Discord from "discord.js";
+import ytdl from "ytdl-core";
+import { getLinkFromInput } from "../util/helperFunctions";
 
 export const executePlayCommand = async (
   interaction: Discord.CommandInteraction<Discord.CacheType>,
 ) => {
   const { guildId, member, options } = interaction;
-  const link = options.getString("link");
-  if (link !== null) {
+  const input = options.getString("link");
+  if (input !== null) {
+    const link = await getLinkFromInput(input);
+    if (link !== undefined) {
+      // TODO: Get song from Url
+    } else {
+      interaction.reply("Sorry, we cound't find a video for you.");
+    }
   } else {
-    interaction.reply("");
+    interaction.reply("Sorry, we couldn't find a YouTube link or search term.");
   }
 };
--- src/util/helperFunctions.ts
+++ src/util/helperFunctions.ts
@@ -1,1 +1,9 @@
+import ytdl from "ytdl-core";
+import yts from "yt-search";
 
+export const getLinkFromInput = async (input: string) => {
+  if (ytdl.validateURL(input)) return input;
+  const results = await yts(input);
+  const result = results.videos.shift();
+  if (result !== undefined) return result.url;
+};
