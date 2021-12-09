e4bc4a5f31a21ee300283eea4b6a1193216e5fda
shyrack <CodeBucketCore@gmail.com>
2021-12-09 23:50:28 +0100

    added info from input function for yt

--- src/util/helperFunctions.ts
+++ src/util/helperFunctions.ts
@@ -1,19 +1,24 @@
 import * as play from "play-dl";
 
 const getInfoFromSearch = async (term: string) => {
-  const youtubeSearchResult = await play.search(term, {
+  const youtubeSearchResults = await play.search(term, {
     source: {
       youtube: "video",
     },
   });
-  return youtubeSearchResult.shift();
+  const result = youtubeSearchResults.shift();
+  const url = result?.url;
+  if (url !== undefined) {
+    return play.video_info(url);
+  }
 };
 
 export const getInfoFromInput = async (input: string) => {
   const validation = await play.validate(input);
-  if (validation === false) return;
-  const info = validation === "search" ? await getInfoFromSearch(input) : await play.deezer(input);
+  if (validation === false || (validation.includes("yt") === false && validation !== "search"))
+    return;
+  const info =
+    validation === "search" ? await getInfoFromSearch(input) : await play.video_info(input);
   if (info === undefined) return;
-  console.log("info", info);
   return info;
 };
