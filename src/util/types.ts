f8434973792aecf3014d869c78ffce0ce168a27b
shyrack <CodeBucketCore@gmail.com>
2021-12-10 22:16:35 +0100

    outsourced types for queue and helperfunctions

---
+++ src/util/types.ts
@@ -0,0 +1,18 @@
+import Discord from "discord.js";
+import DiscordVoice from "@discordjs/voice";
+import { InfoData } from "play-dl/dist/YouTube/utils/constants";
+
+export type Song = {
+  infoData: InfoData;
+  isLive: boolean;
+  member: Discord.GuildMember;
+};
+
+export type MusicQueue = {
+  audioPlayer: DiscordVoice.AudioPlayer;
+  currentSong: Song | null;
+  guildId: string;
+  isPaused: boolean;
+  songs: Song[];
+  voiceConnection: DiscordVoice.VoiceConnection | null;
+};
