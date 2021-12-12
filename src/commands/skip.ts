76c2f196064c283c8b2b5f99df2088eede5826ef
shyrack <CodeBucketCore@gmail.com>
2021-12-12 19:34:47 +0100

    removed some useless code in skip command

--- src/commands/skip.ts
+++ src/commands/skip.ts
@@ -4,12 +4,8 @@ import { Queue } from "../queue/queue";
 export const executeSkipCommand = async (
   interaction: Discord.CommandInteraction<Discord.CacheType>,
 ) => {
-  const { guildId, member } = interaction;
-  if (member instanceof Discord.GuildMember) {
-    const musicQueue = Queue.getMusicQueue(guildId);
-    musicQueue.skip();
-    interaction.reply("The current song has been skipped.");
-    return;
-  }
-  interaction.reply("Sorry, we couldn't skip the current song.");
+  const { guildId } = interaction;
+  const musicQueue = Queue.getMusicQueue(guildId);
+  musicQueue.skip();
+  interaction.reply("The current song has been skipped.");
 };
