e413a4cb9cb3680fac4f6076c0c0e3fb4c2b45a0
shyrack <CodeBucketCore@gmail.com>
2021-12-22 00:21:38 +0100

    removed types and use class instead

--- src/util/types.ts
+++ src/util/types.ts
@@ -1,22 +1,6 @@
 import Discord from "discord.js";
 import * as DiscordVoice from "@discordjs/voice";
-
-export type Playable = {
-  channel: string;
-  member: Discord.GuildMember;
-  thumbnailUrl: string;
-  title: string;
-  uploadedAt: string;
-  url: string;
-};
-
-export type PlayableList = Playable & {
-  urls: string[];
-};
-
-export type PlayableSong = Playable & {
-  isLive: boolean;
-};
+import { Playable } from "../playable/Playable";
 
 export type MusicQueue = {
   audioPlayer: DiscordVoice.AudioPlayer;
