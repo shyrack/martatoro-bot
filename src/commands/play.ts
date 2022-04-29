ec143e1b82e56c880b3fe148d181fa1405d8a4fd
shyrack <CodeBucketCore@gmail.com>
2022-04-29 22:14:33 +0200

    fixed message bug when client is not in channel

--- src/commands/play.ts
+++ src/commands/play.ts
@@ -3,9 +3,7 @@ import Discord from "discord.js";
 import { Queue } from "../queue/queue";
 import { embedFromPlayable, playableFromInput } from "../util/helperFunctions";
 
-export const executePlayCommand = async (
-  interaction: Discord.CommandInteraction<Discord.CacheType>,
-) => {
+export const executePlayCommand = async (interaction: Discord.CommandInteraction<Discord.CacheType>) => {
   const { guildId, member, options } = interaction;
   const input = options.getString("input");
   if (input !== null && member instanceof Discord.GuildMember) {
@@ -22,13 +20,16 @@ export const executePlayCommand = async (
             ? "Song or playlist will now be played."
             : "Song or playlist was added to the queue.",
           member,
-          _.size(musicQueue.playables),
+          _.size(musicQueue.playables)
         );
         interaction.reply({
-          embeds: [embed],
+          embeds: [embed]
         });
         return;
       }
+    } else {
+      interaction.reply("Sorry, but you have to join a voice channel first.");
+      return;
     }
   }
   interaction.reply("Sorry, we couldn't find a YouTube link or search term.");
